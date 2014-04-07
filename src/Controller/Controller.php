5b2de2e6611aaf9eaa9938c7e892313a8ac25ff0
Lukas Wilkeer <lukasc4l@gmail.com>
2014-04-07 17:29:05 -0300

    Controller class.

---
+++ src/Controller/Controller.php
@@ -0,0 +1,21 @@
+<?php
+namespace Controller;
+use Silex\Application;
+use Mustache\Silex\Provider\MustacheServiceProvider;
+
+class Controller
+{
+    public static $app;
+    var $tpl;
+    
+    function __construct()
+    {
+        $this->app = new Application();
+        $this->app->register(new MustacheServiceProvider(), array(
+            'mustache.path'    => APPDIR.'/view',
+            'mustache.partials_path' => APPDIR.'/view', 
+            'mustache.options' => array('cache' => APPDIR.'/cache/mustache'),
+        ));
+    } 
+}
+?>

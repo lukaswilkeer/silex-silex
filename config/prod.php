df39c6bfeb8c349549e3d87c7858af79ea2fd6e6
Lukas Wilkeer <lukasc4l@gmail.com>
2014-04-07 16:32:28 -0300

    COnfiguração do servidor de produção.

--- config/prod.php
+++ config/prod.php
@@ -1,3 +1,23 @@
 <?php
 
-// configure your app for the production environment
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\WebProfilerServiceProvider;
+
+// enable ini set erros
+ini_set('display_errors',1);
+
+// System enviroments variables.
+define("APPDIR", $_SERVER['DOCUMENT_ROOT'].'/opensource/silex-silicon');
+define("URL","http://www.myapp.com");
+
+// enable the debug mode
+$app['debug'] = false;
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../logs/silex_dev.log',
+));
+
+$app->register($p = new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../cache/profiler',
+));
+$app->mount('/_profiler', $p);
